2d9ec97584e8f0319fd2426485de717c3964f065
Michael Edwards <michael.edwards@ennismore.com>
2024-12-02 15:23:50 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Proposal Manager
+A WordPress plugin to manage proposals with custom metadata for prices.
---
+++ proposal-manager.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Proposal Manager
+ * Description: A WordPress plugin to manage proposals with custom metadata for prices.
+ * Version: 1.0
+ * Author: Michael Edwards
+ * Text Domain: proposal-manager
+ */
+
+defined('ABSPATH') || exit; // Exit if accessed directly.
